8dfde4ad6de48b64fcf68fcae7e4c2376279c2a3
Aithne Morgana <MxAithne@users.noreply.github.com>
2020-07-14 16:54:24 +0100

    Got file reading from storage

--- fuel-economy-analysis/InputToDataStore.cs
+++ fuel-economy-analysis/InputToDataStore.cs
@@ -1,11 +1,15 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.Storage;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Newtonsoft.Json;
 using Microsoft.VisualBasic;
 
@@ -16,13 +20,16 @@ namespace fuel_economy_analysis
         [FunctionName("InputToDataStore")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [Blob("azure-webjobs-hosts/fueldata.csv", FileAccess.Read, Connection = "")] Stream myBlob,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
+
             string error = "Error: 400";
             bool failed = false;
             string key = req.Query["key"];
+            string secKey = "Change";
             string dateIn = req.Query["date"];
             string odoIn = req.Query["odo"];
             string carMPGIn = req.Query["carMPG"];
@@ -42,6 +49,12 @@ namespace fuel_economy_analysis
 
             DateTime date;
             decimal odo, carMPG, fuel;
+
+            if (key != secKey)
+            {
+                failed = true;
+                error += System.Environment.NewLine + "Invalid Key";
+            }
 
             if (!DateTime.TryParse(dateIn, out date))
             {
@@ -71,12 +84,18 @@ namespace fuel_economy_analysis
 
             if (!failed)
             {
+                StreamReader reader = new StreamReader(myBlob);
+                string datafile = reader.ReadToEnd();
+
                 // If no errors in data recieved echo back
                 responseMessage = "Got data:" + System.Environment.NewLine +
                     $"Date: {dateIn}" + System.Environment.NewLine +
                     $"ODO: {odo}" + System.Environment.NewLine +
-                    $"CarMPG: {carMPG}" + System.Environment.NewLine +
-                    $"Fuel: {fuel}";
+                    $"Car MPG: {carMPG}" + System.Environment.NewLine +
+                    "Car KPL: " + MPGtoKPL(carMPG) + System.Environment.NewLine +
+                    "Car l/100km: " + MPGtoL100(carMPG) + System.Environment.NewLine +
+                    $"Fuel: {fuel}" + System.Environment.NewLine +
+                    datafile;
 
             }
             else
@@ -89,12 +108,28 @@ namespace fuel_economy_analysis
 
         }
 
+        public static decimal MPGtoKPL(decimal mpg)
+        {
+            decimal conversionFactor = 2.82481061M;
+            decimal kpl = decimal.Divide(mpg, conversionFactor);
+            return kpl;
+        }
+
+        public static decimal MPGtoL100(decimal mpg)
+        {
+            decimal conversionFactor = 282.481061M;
+            decimal l100 = decimal.Divide(conversionFactor, mpg);
+            return l100;
+        }
+
+        public static decimal MilestoKM(decimal miles)
+        {
+            decimal conversionFactor = 1.609344M;
+            decimal kilometers = decimal.Multiply(miles, conversionFactor);
+            return kilometers;
+        }
+
     }
 
-    /*public static void MPGtoKPL(decimal mpg)
-    {
-        decimal conversionFactor = 0.354;
-        decimal kpl = decimal.Multiply(mpg, conversionFactor);
-        return kpl;
-    }*/
+
 }
